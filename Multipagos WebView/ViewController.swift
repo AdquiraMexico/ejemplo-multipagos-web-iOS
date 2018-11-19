05fc1cf8d299011836b412586f858a1947dcc52a
Daniel <daniel@digitalcoaster.mx>
2018-11-19 13:55:48 -0600

    Venga

--- Multipagos WebView/ViewController.swift
+++ Multipagos WebView/ViewController.swift
@@ -8,8 +8,65 @@
 
 import UIKit
 
+
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Lanzar: UIButton!
+    
+    @IBAction func invocar(_ sender: Any) {
+        
+
+        let parameters = ["id": 13, "name": "jack"] as [String : Any]
+        
+        //create the url with URL
+        let url = URL(string: "www.thisismylink.com/postName.php")! //change the url
+        
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
